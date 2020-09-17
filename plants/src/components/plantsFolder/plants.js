c5a9aa43aec2283a70de40c1d2657e4719df6d97
juliusheese <julius.42.shapiro@gmail.com>
2020-09-17 18:35:26 -0500

    const Plant created

--- plants/src/components/plantsFolder/plants.js
+++ plants/src/components/plantsFolder/plants.js
@@ -2,3 +2,15 @@ import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
 import axios from 'axios';
 import styled from 'styled-components';
+
+const Plant = () => {
+
+    const [PlantState, setPlantState] = useState({
+        id: '',
+        nickname: '',
+        species: '',
+        h2oFrequency: ''
+    });
+}
+
+export default Plant
