48710d5a62dec279c8f1170fab53c1346089bedd
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-09-17 18:12:01 -0700

    built the mock api call. will fix when we get backend

---
+++ plants/src/api/fetchPlants.js
@@ -0,0 +1,15 @@
+// import axiosWithAuth from "../utils/axiosWithAuth";
+
+
+// export const fetchPlants = () => {
+//     return axiosWithAuth()
+//     .get("http://localhost:300")
+//     .then(response => {
+//             console.log(response);
+//             return response;
+//     })
+//     .catch(error => {
+//         console.log(error.message);
+//         return error;
+//     })
+// }
