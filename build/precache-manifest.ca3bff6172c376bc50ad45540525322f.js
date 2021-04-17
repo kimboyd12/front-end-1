ed0ba764aba80b7fb96c819518bf51d4fbe68c76
kim-boyd <kimberlyboyd2@gmail.com>
2021-04-16 21:03:01 -0400

    build

---
+++ build/precache-manifest.ca3bff6172c376bc50ad45540525322f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255a210f44829cebee5ddddc4fb7a351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e45a28ac293f681d369",
+    "url": "/static/css/main.803b116f.chunk.css"
+  },
+  {
+    "revision": "e45a15edc7bad21f4348",
+    "url": "/static/js/2.d336f82c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d336f82c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e45a28ac293f681d369",
+    "url": "/static/js/main.318506e3.chunk.js"
+  },
+  {
+    "revision": "232ea06dc53a11d0a054",
+    "url": "/static/js/runtime-main.238dbb1e.js"
+  },
+  {
+    "revision": "f6ea824b563e99360079425cff407659",
+    "url": "/static/media/Logo.f6ea824b.png"
+  }
+]);
